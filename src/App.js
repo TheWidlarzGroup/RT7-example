4f907e8a9db79321ed950670475e2e4f3eb6011c
Bartek Bajda <bart.bajda@thewidlarzgroup.com>
2020-04-13 21:02:58 +0200

    Fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const doFetch = async () => {
+      const response = await fetch('https://randomuser.me/api/?results=100');
+      const body = await response.json();
+      const contacts = body.results;
+      console.log(contacts);
+      setData(contacts);
+    };
+    doFetch();
+  }, []);
+
   return <div>Hello</div>;
 };
 
